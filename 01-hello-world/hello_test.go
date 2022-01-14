362ee42a84bbcddbf3903cd69c532603f4a0cecb
hritesh93 <hriteshbebop@gmail.com>
2022-01-14 14:43:40 +0530

    add subtests

--- 01-hello-world/hello_test.go
+++ 01-hello-world/hello_test.go
@@ -3,10 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Zenitsu")
-	want := "Hello, Zenitsu"
-
-	if got != want {
-		t.Errorf("Got: %q | Want: %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("Got: %q | Want: %q", got, want)
+		}
 	}
+
+	t.Run("Saying hello to demons", func(t *testing.T) {
+		got := Hello("Inosuke")
+		want := "Hello, Inosuke"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
