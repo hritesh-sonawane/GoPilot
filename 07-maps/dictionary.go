92220df04ab01085c809f3034b78c192039753c0
hritesh93 <hriteshbebop@gmail.com>
2022-01-20 22:07:53 +0530

    add dict search /map

---
+++ 07-maps/dictionary.go
@@ -0,0 +1,16 @@
+package dictionary
+
+import "errors"
+
+var ErrNotFound = errors.New("word not found")
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
