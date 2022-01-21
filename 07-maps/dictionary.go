11f41beb87a569bd53925a4332a0c76d2b116624
hritesh93 <hriteshbebop@gmail.com>
2022-01-21 10:43:32 +0530

    add/update/del & error as const

--- 07-maps/dictionary.go
+++ 07-maps/dictionary.go
@@ -1,10 +1,18 @@
 package dictionary
 
-import "errors"
-
-var ErrNotFound = errors.New("word not found")
+var (
+	ErrNotFound         = DictionaryErr("word not found❌")
+	ErrWordExists       = DictionaryErr("word already exists❌")
+	ErrWordDoesNotExist = DictionaryErr("word does not exist❌")
+)
 
 type Dictionary map[string]string
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
@@ -13,4 +21,38 @@ func (d Dictionary) Search(word string) (string, error) {
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
