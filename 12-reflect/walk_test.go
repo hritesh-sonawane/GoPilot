a403bc6cdfc5811afb49ec3e2b1569434368327c
hritesh93 <hr7tesh@gmail.com>
2022-01-28 17:59:02 +0530

    recursion to traverse arbitrary data structures
    do your best to avoid using reflection🥴

--- 12-reflect/walk_test.go
+++ 12-reflect/walk_test.go
@@ -69,7 +69,75 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Japan", "China"},
 		},
+		{
+			"Arrays",
+			[2]Profile{
+				{14, "Japan"},
+				{17, "China"},
+			},
+			[]string{"Japan", "China"},
+		},
 	}
+	// {
+	// 	"Maps",
+	// 	map[string]string{
+	// 		"Foo": "Bar",
+	// 		"Baz": "Boz",
+	// 	},
+	// 	[]string{"Bar", "Boz"},
+	// },
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{14, "Japan"}
+			aChannel <- Profile{17, "China"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Japan", "China"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v | want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{14, "Japan"}, Profile{17, "China"}
+		}
+
+		var got []string
+		want := []string{"Japan", "China"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v | want %v", got, want)
+		}
+	})
 
 	for _, test := range cases {
 		t.Run(test.Name, func(t *testing.T) {
@@ -84,4 +152,17 @@ func TestWalk(t *testing.T) {
 		})
 	}
 
+}
+
+func assertContains(t testing.TB, haystack []string, needle string) {
+	t.Helper()
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
+	}
 }
