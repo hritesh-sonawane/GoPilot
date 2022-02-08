6e7aec28818f5f713237cb2a59fe1eebe32b0a54
hritesh93 <hr7tesh@gmail.com>
2022-02-08 22:45:19 +0530

    analog clock--base
    secHand, secInRad, secAt30

---
+++ 16-math/clock_test.go
@@ -0,0 +1,52 @@
+package clock
+
+import (
+	"math"
+	"testing"
+	"time"
+
+	"github.com/quii/learn-go-with-tests/math/v1/clockface"
+)
+
+func TestSecondsInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 0, 30), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(0, 0, 45), (math.Pi / 2) * 3},
+		{simpleTime(0, 0, 7), (math.Pi / 30) * 7},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondsInRadians(c.time)
+			if got != c.angle {
+				t.Fatalf("want %v radians, got %v", c.angle, got)
+			}
+		})
+	}
+}
+
+func TestSecondHandAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 - 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v | want %v", got, want)
+	}
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := clockface.Point{X: 150, Y: 150 + 90}
+	got := clockface.SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v | want %v", got, want)
+	}
+}
