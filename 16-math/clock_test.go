33cee7877f8dc63c9e80be924ac8d5c42d2e3465
hritesh93 <hr7tesh@gmail.com>
2022-02-10 23:29:40 +0530

    add seconds hand +/test

--- 16-math/clock_test.go
+++ 16-math/clock_test.go
@@ -4,8 +4,6 @@ import (
 	"math"
 	"testing"
 	"time"
-
-	"github.com/quii/learn-go-with-tests/math/v1/clockface"
 )
 
 func TestSecondsInRadians(t *testing.T) {
@@ -23,30 +21,45 @@ func TestSecondsInRadians(t *testing.T) {
 		t.Run(testName(c.time), func(t *testing.T) {
 			got := secondsInRadians(c.time)
 			if got != c.angle {
-				t.Fatalf("want %v radians, got %v", c.angle, got)
+				t.Fatalf("got %v | want %v", c.angle, got)
 			}
 		})
 	}
 }
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
+func TestSecondHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 0, 30), Point{0, -1}},
+		{simpleTime(0, 0, 45), Point{-1, 0}},
+	}
 
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("got %v | want %v", got, want)
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := secondHandPoint(c.time)
+			if !roughlyEqualPoint(got, c.point) {
+				t.Fatalf("got %v | want %v", got, c.point)
+			}
+		})
 	}
 }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
 
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(tm)
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
+}
 
-	if got != want {
-		t.Errorf("got %v | want %v", got, want)
-	}
+func roughlyEqualFloat64(a, b float64) bool {
+	const equalityThreshold = 1e-7
+	return math.Abs(a-b) < equalityThreshold
+}
+
+func roughlyEqualPoint(a, b Point) bool {
+	return roughlyEqualFloat64(a.X, b.X) &&
+		roughlyEqualFloat64(a.Y, b.Y)
 }
